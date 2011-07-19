765c4671a92c0f5acd5d2dcfcacb677351644b4c
Nick Malcolm <nick@southgatelabs.com>
2011-07-19 16:39:02 +1200

    Proper stats

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,6 +3,12 @@ class StatisticsController < ApplicationController
   def index
     @used   = current_shop.discounts.used
     @unused  = current_shop.discounts.unused
+    arr = []
+    Order.sum(:total_price, :group => "DATE(created_at)", :conditions => "discount_code IS NOT NULL").each {|k,v| arr << ["#{k.strftime("%d-%b-%y")}", v.to_f]}
+    @sales_value = arr
+    arr = []
+    Order.count(:group => "DATE(created_at)", :conditions => "discount_code IS NOT NULL").each {|k,v| arr << ["#{k.strftime("%d-%b-%y")}", v.to_i]}
+    @sales_count = arr
   end
   
 end
